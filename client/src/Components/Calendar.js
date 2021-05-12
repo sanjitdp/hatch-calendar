32fca787fc6fafc49e2507d653d095d53041f3c3
andersonxanna <annaranderson@icloud.com>
2021-05-11 22:39:36 -0700

    feat: added calendar component and styling file, installed date-fns | fix: index.js configuration to render

---
+++ client/src/Components/Calendar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import * as dateFns from "date-fns";
+
+class Calendar extends React.Component {
+    render() {
+      return (
+        <p>I am Calendar</p>
+      );
+    }
+}
+  
+export default Calendar;
