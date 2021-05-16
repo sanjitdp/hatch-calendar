00d60da95a9226e51eeaf50bf0abb77ea5829283
Katherine Stahnke <katherinestahnke@s-169-232-245-50.resnet.ucla.edu>
2021-05-16 16:58:29 -0700

    initial day page upload to figure out git and check what's working

--- client/src/Components/Day.js
+++ client/src/Components/Day.js
@@ -1,9 +1,39 @@
 import React from "react";
 
 class Day extends React.Component {
+    renderHeader() {
+        return (
+            <div> THIS IS THE DAY PAGE </div>
+            // needs prev and next buttons
+            // load date in header
+            // load day of the week here
+        )
+    }
+    renderEvent() {
+        return (
+            <div> EVENTS </div>
+            // generate times (...idk how to do this)
+            // write TIMES and EVENTS as header
+            // load events (?)
+        )
+    }
+    renderOptions() {
+        return (
+            <div> export csv / add event</div>
+            // export as csv button (this will have to be a separate thing)
+            // add event button
+            // go to date 
+        )
+    }    
+    renderFooter() {
+        return(
+            <div> back to calendar </div>
+            // return to calendar button
+        )
+    }
     render() {
         return (
-            <div>This is day page</div>
+            <div>This is the DAY page</div>
         )
     }
 }
