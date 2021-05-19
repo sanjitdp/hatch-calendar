1499ed30202926a540dde2242d115f1c4ed29375
Nitya Simhadri <nityasimhadri@Kirans-MacBook-Pro.local>
2021-05-19 12:11:15 -0500

    finished formatting

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import egg from '../images/hatched_egg.png';
-  import './Home.css';
+import './Home.css';
+
 class Home extends React.Component {
     render() {
         return (
@@ -10,18 +11,18 @@ class Home extends React.Component {
 		HATCH
 		</p>
 		
-     <p>
-Welcome to <span className="inlinehatch"> Hatch </span>,the interactive calendar for all your scheduling needs. <br />
-We invite you to come hatch your plans with us and see your goals come to fruition. 
-</p>
-<a href="/login"> login </a>
-<p>
-Don't have an account?
-<a href="/register"> Register </a>
-</p>
- </div>
-	 )
-    }
+	        <p>
+		Welcome to <span className="inlinehatch">HATCH</span>, the interactive calendar for all your scheduling needs. <br />
+		We invite you to come hatch your plans with us and see your goals come to fruition. 
+		</p>
+		<a href="/login"> login </a>
+		<p>
+		Don't have an account?  
+		<a href="/register"> Register </a>
+		</p>
+	 </div>
+		 )
+    	}
 }
 
 export default Home;
